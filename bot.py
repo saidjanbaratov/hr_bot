545bbfe022ae7d36b4da2210e1deb236288e6582
saidjanbaratov <saidjanbaratov@gmail.com>
2026-05-23 18:14:46 +0500

    Update bot.py
    Signed-off-by: saidjanbaratov <saidjanbaratov@gmail.com>

--- bot.py
+++ bot.py
@@ -3,11 +3,7 @@ import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import (
-    Message,
-    ReplyKeyboardMarkup,
-    KeyboardButton
-)
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,6 +12,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+users = {}
 
 menu = ReplyKeyboardMarkup(
     keyboard=[
@@ -29,30 +27,45 @@ menu = ReplyKeyboardMarkup(
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer(
-        "Assalomu alaykum 👋\n"
-        "HR bo‘limi botiga xush kelibsiz.",
+        "Assalomu alaykum 👋\nHR bo‘limi botiga xush kelibsiz.",
         reply_markup=menu
     )
 
 
 @dp.message()
 async def handler(message: Message):
+    user_id = message.from_user.id
+    text = message.text
 
-    if message.text == "📞 Aloqa":
-        await message.answer(
-            "📞 HR telefon:\n"
-            "+998 50 003 26 55"
-        )
+    if text == "📞 Aloqa":
+        await message.answer("📞 HR telefon:\n+998 50 003 26 55")
+        return
 
-    elif message.text == "📝 Ariza topshirish":
-        await message.answer(
-            "👤 Ism familiyangizni yozing:"
-        )
+    if text == "📝 Ariza topshirish":
+        users[user_id] = {"step": "name"}
+        await message.answer("👤 Ism familiyangizni yozing:")
+        return
 
-    else:
-        await message.answer(
-            "Pastdagi tugmalardan foydalaning 👇"
-        )
+    if user_id in users:
+        step = users[user_id]["step"]
+
+        if step == "name":
+            users[user_id]["name"] = text
+            users[user_id]["step"] = "age"
+            await message.answer("🎂 Yoshingizni yozing:")
+            return
+
+        if step == "age":
+            users[user_id]["age"] = text
+            await message.answer(
+                f"✅ Ma’lumot qabul qilindi:\n\n"
+                f"👤 Ism: {users[user_id]['name']}\n"
+                f"🎂 Yosh: {users[user_id]['age']}"
+            )
+            del users[user_id]
+            return
+
+    await message.answer("Pastdagi tugmalardan foydalaning 👇", reply_markup=menu)
 
 
 async def main():
